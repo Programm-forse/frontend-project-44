03abf6f0e4b24b97eb084bc8c833ce7e93b80626
darksider <expedition500@gmail.com>
2022-11-27 00:45:03 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -40,35 +40,3 @@ export const generateRandomNumber = (n) => {
   const randomNumber = Math.floor(Math.random() * n);
   return randomNumber;
 };
-
-const basisOfGames = (noteToGame, task) => {
-  console.log('Welcome to the Brain Games!');
-
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  // Вывод на экран адание (в чём суть игры)
-  console.log(noteToGame);
-
-  // Начало цикла игры
-  for (let i = 0; i < 3; i += 1) {
-    const pairOfQuestionAndResult = task();
-    console.log('Question: ', pairOfQuestionAndResult[0]);
-    const answerUser = readlineSync.question('Your answer: ');
-
-    // Константа для правильного ответа
-    const rightAnswer = pairOfQuestionAndResult[1];
-
-    // Проверка
-    if (answerUser === rightAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answerUser}" is wrong answer ;(. Correct answer was "${rightAnswer}". \nLet's try again, ${userName}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${userName}!`);
-};
-
-export default basisOfGames;
